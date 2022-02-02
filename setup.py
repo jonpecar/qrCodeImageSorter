f0b62c7e9d3a1494119e288f492e9d323ed82e8b
Jonathan Pecar <jonathan.pecar@outlook.com>
2022-02-02 19:05:31 +1100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='qrImageIndexer',
+    version='0.1',
+    packages=['.qr_image_indexer'],
+    install_requires=[
+        'pyzbar[PIL]',
+        'pillow',
+        'reportlab',
+        'qrcode'
+    ]
+)
