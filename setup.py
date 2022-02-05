120e9481ea4e9a8e25a4d19f1aa665bcd0f7cb60
Jonathan Pecar <jonathan.pecar@outlook.com>
2022-02-05 18:07:23 +1100

    Updated setup file and built latest version

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
 setup(
+    requires=[
+        'setuptools',
+        'wheel'
+    ],
     name='qrImageIndexer',
-    version='0.1',
-    packages=['.qr_image_indexer'],
+    version='0.1.1',
+    author='Jonathan Pecar',
+    author_email='Jonathan.Pecar@gmail.com',
+    description='Tool for indexing images with QR codes',
+    packages=['qr_image_indexer'],
     install_requires=[
-        'pyzbar[PIL]',
+        'pyzbar',
         'pillow',
         'reportlab',
         'qrcode'
