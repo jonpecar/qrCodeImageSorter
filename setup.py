39fcbbd54a690459dfddb243b8e113dc19e94d53
Jonathan Pecar <jonathan.pecar@outlook.com>
2022-03-30 09:17:14 +1100

    Increased project revision.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
         'wheel'
     ],
     name='qrImageIndexer',
-    version='0.2.0',
+    version='0.2.1',
     author='Jonathan Pecar',
     author_email='Jonathan.Pecar@gmail.com',
     description='Tool for indexing images with QR codes',
