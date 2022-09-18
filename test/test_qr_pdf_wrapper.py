d15bec57c71ad3bc2bb367a5ed5ddaf92975b6c8
Jonathan Pecar <jonathan.pecar@outlook.com>
2022-09-18 12:24:13 +1000

    Added tests for qr generate wrapper function

--- test/test_qr_pdf_wrapper.py
+++ test/test_qr_pdf_wrapper.py
@@ -2,17 +2,17 @@ from qrImageIndexer import generate_qr_wrapper
 from unittest import mock
 
 def mock_qr_generator_functions(mocker : mock):
-    unpack_data = mocker.patch('qrImageIndexer.qr_generator.unpack_data')
-    generate_qr_structure = mocker.patch('qrImageIndexer.qr_generator.generate_qr_code_structure')
-    build_pdf = mocker.patch('qrImageIndexer.write_pdf_fpf2.build_pdf_report')
+    unpack_data = mocker.patch('qrImageIndexer.generate_qr_wrapper.unpack_data')
+    generate_qr_structure = mocker.patch('qrImageIndexer.generate_qr_wrapper.generate_qr_code_structure')
+    build_pdf = mocker.patch('qrImageIndexer.generate_qr_wrapper.build_pdf_report')
 
     return (unpack_data, generate_qr_structure, build_pdf)
 
 
 def test_single_func_pdf_builder_all_true(mocker):
     (unpack_data, generate_qr_structure, build_pdf) = mock_qr_generator_functions(mocker)
-    result = generate_qr_wrapper.generate_qr_pdf([['Line 1', ''], ['', 'Line 1 Indent']], r'{header}',
-                                        True, True, True)
+    result = generate_qr_wrapper.generate_qr_pdf([['Line 1', ''], ['', 'Line 1 Indent']],
+                                        True, True, True, r'{header}')
     
     unpack_data.assert_called_once_with([['Line 1', ''], ['', 'Line 1 Indent']], True, r'{header}')
     generate_qr_structure.assert_called_once_with(unpack_data.return_value)
@@ -21,8 +21,8 @@ def test_single_func_pdf_builder_all_true(mocker):
 
 def test_single_func_pdf_builder_no_qr_heading(mocker):
     (unpack_data, generate_qr_structure, build_pdf) = mock_qr_generator_functions(mocker)
-    result = generate_qr_wrapper.generate_qr_pdf([['Line 1', ''], ['', 'Line 1 Indent']], r'{header}',
-                                        False, True, True)
+    result = generate_qr_wrapper.generate_qr_pdf([['Line 1', ''], ['', 'Line 1 Indent']],
+                                        False, True, True, r'{header}')
     
     unpack_data.assert_called_once_with([['Line 1', ''], ['', 'Line 1 Indent']], False, r'{header}')
     generate_qr_structure.assert_called_once_with(unpack_data.return_value)
@@ -31,8 +31,8 @@ def test_single_func_pdf_builder_no_qr_heading(mocker):
 
 def test_single_func_pdf_builder_no_title_all_heading(mocker):
     (unpack_data, generate_qr_structure, build_pdf) = mock_qr_generator_functions(mocker)
-    result = generate_qr_wrapper.generate_qr_pdf([['Line 1', ''], ['', 'Line 1 Indent']], r'{header}',
-                                        True, False, True)
+    result = generate_qr_wrapper.generate_qr_pdf([['Line 1', ''], ['', 'Line 1 Indent']],
+                                        True, False, True, r'{header}')
     
     unpack_data.assert_called_once_with([['Line 1', ''], ['', 'Line 1 Indent']], True, r'{header}')
     generate_qr_structure.assert_called_once_with(unpack_data.return_value)
@@ -41,8 +41,8 @@ def test_single_func_pdf_builder_no_title_all_heading(mocker):
 
 def test_single_func_pdf_builder_not_sliceable(mocker):
     (unpack_data, generate_qr_structure, build_pdf) = mock_qr_generator_functions(mocker)
-    result = generate_qr_wrapper.generate_qr_pdf([['Line 1', ''], ['', 'Line 1 Indent']], r'{header}',
-                                        True, True, False)
+    result = generate_qr_wrapper.generate_qr_pdf([['Line 1', ''], ['', 'Line 1 Indent']],
+                                        True, True, False, r'{header}')
     
     unpack_data.assert_called_once_with([['Line 1', ''], ['', 'Line 1 Indent']], True, r'{header}')
     generate_qr_structure.assert_called_once_with(unpack_data.return_value)
